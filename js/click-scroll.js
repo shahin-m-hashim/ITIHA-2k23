3131a9211b9e00bba7a524f209fb3ced973b23d7
Shahin M Hashim <shahinmhashim@gmail.com>
2023-04-14 15:08:09 +0530

    added new section for sponsors and also modified proshow file to be more visually appealing

--- js/click-scroll.js
+++ js/click-scroll.js
@@ -1,7 +1,7 @@
 //jquery-click-scroll
 //by syamsul'isul' Arifin
 
-var sectionArray = [1, 2, 3, 4, 5, 6];
+var sectionArray = [1, 2, 3, 4, 5, 6, 7];
 
 $.each(sectionArray, function(index, value){
           
